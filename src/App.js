9a645bb976b3c125cc4512094bc15cd39c784a62
estevansta <cruz@therrb.co>
2019-04-23 23:18:46 -0500

    navbar showing on react app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Navbar from "./components/navbar";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Navbar/>
       <header className="App-header">
         <div className="jumbotron">
           <h1>Clicky Game</h1>
---
+++ src/components/navbar.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Navbar() {
+return (
+    <nav className="navbar text-center text-nowrap">
+        <h1>Clicky Game Navbar</h1>
+        <h2 className="text-right text-nowrap">Score: </h2>
+    </nav>
+    );
+}
+
+export default Navbar
