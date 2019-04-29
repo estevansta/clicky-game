27e25c7bc5471bfcc23fc6a6d4cf63cfc2513809
estevansta <cruz@therrb.co>
2019-04-29 10:20:12 -0500

    score populated with 0 using state and props

--- src/App.js
+++ src/App.js
@@ -3,30 +3,30 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from "./components/navbar";
 import Container from "./components/container";
-// import Spongebob from  "./spongebob.json"
+import Spongebob from  "./spongebob.json"
 
 // From function App() to class App extends Component to import components
 class App extends Component {
-
+  // setting up counter and score tracker
+  state = {
+    Spongebob,
+    counter: 0,
+    score:0,
+  }
 
   // render instead of function to display DOM
   render() {
     return (
       <div className="container">
-        <Navbar/>
+        {/* state counter gets passed into the Navb ar */}
+        <Navbar 
+        counter={this.state.counter}
+        score={this.state.score}/>
         <header className="App-header">
           <div className="jumbotron">
             <h1>Clicky Game</h1>
             <p>Hello World</p>
             <Container/>
-            {/* {this.state.Spongebob.map(spongebob => (
-            <Container
-            id={spongebob.id}
-            name={spongebob.name}
-            image={spongebob.image}
-            clicked={spongebob.clicked}
-          />
-        ))} */}
             
           </div>
         </header>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,10 @@
 import React from "react"
 
-function Navbar() {
+function Navbar(props) {
 return (
     <nav className="navbar">
         <h2>Clicky Game Navbar</h2>
-        <h2 className="score">Score: 0 </h2>
+        <h2 className="score">Score: {props.counter} </h2>
     
     </nav>
     );
